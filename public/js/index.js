918b93b8aa366318d0e289e8c80fba68357ea0c3
EndreoT <endreotm@gmail.com>
2019-06-19 14:46:50 -0700

    change production url

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,5 @@
 // const developmentBaseUrl = 'http://localhost:3000/';
-const productionBaseUrl = 'https://vast-gorge-37663.herokuapp.com/';
+const productionBaseUrl = 'https://divvy-app.herokuapp.com/';
 
 // const baseUrl = developmentBaseUrl;
 const baseUrl = productionBaseUrl;
